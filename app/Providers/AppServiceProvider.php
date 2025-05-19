a8708441e7a856518e7332ab49f9819b39f7f0ef
Jesus Dominguez <romandominguezgarcia.01@gmail.com>
2025-05-18 20:17:02 -0700

    solving styles on dev and prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,7 +19,10 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
-        //
+        {
+        // Usa secure_asset() solo en producción
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') | tork </title>
-    <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     {{-- <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin> --}}
     {{-- <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" crossorigin>
@@ -19,6 +19,6 @@
         @include('layouts.footer')
     </main>
     
-    <script src="{{ secure_asset('css/main.js') }}"></script>
+    <script src="{{ asset('css/main.js') }}"></script>
 </body>
 </html>
