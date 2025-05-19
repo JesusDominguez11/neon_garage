559f265cd1eb5f1333910aecb0827f8f52b1cf5c
Jesus Dominguez <romandominguezgarcia.01@gmail.com>
2025-05-18 19:05:35 -0700

    solved console confrict in js interpreted

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') | tork </title>
-    <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    {{-- <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin> --}}
+    {{-- <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&family=Playfair+Display:wght@400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body class="bg-gray-50 font-sans antialiased">
     @include('layouts.header')
@@ -19,6 +19,6 @@
         @include('layouts.footer')
     </main>
     
-    <script src="{{ secure_asset('css/main.js') }}"></script>
+    <script src="{{ asset('css/main.js') }}"></script>
 </body>
 </html>
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -54,7 +54,7 @@
 </footer>
 
 <!-- FontAwesome -->
-<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+{{-- <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script> --}}
 
 
 
