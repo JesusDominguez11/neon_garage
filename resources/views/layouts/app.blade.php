ac68a8a0211e00a1945ee8c6b80a48da4ff85194
Jesus Dominguez <romandominguezgarcia.01@gmail.com>
2025-05-18 18:45:54 -0700

    modified secure_assets on app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') | tork </title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&family=Playfair+Display:wght@400;500;600;700&display=swap" rel="stylesheet">
@@ -19,6 +19,6 @@
         @include('layouts.footer')
     </main>
     
-    <script src="{{ asset('css/main.js') }}"></script>
+    <script src="{{ secure_asset('css/main.js') }}"></script>
 </body>
 </html>
